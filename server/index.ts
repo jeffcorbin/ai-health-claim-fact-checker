7bf1195a400650ed925e5a56257b99f9c2ca730e
Jeff Corbin <jeffcorbinjr@gmail.com>
2025-06-19 15:03:53 -0500

    update

--- server/index.ts
+++ server/index.ts
@@ -19,7 +19,7 @@ app.post("/check", async (req, res) => {
   }
 });
 
-const PORT = 3000;
+const PORT = 5174;
 app.listen(PORT, () => {
   console.log(`✅ Server is running at http://localhost:${PORT}`);
 });
